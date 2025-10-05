a576a9132ce50b4d64305ce65a1fff988b2984bc
olivax28 <olivax28@outlook.com>
2025-10-04 20:20:42 -0400

    tests for project01

--- Projects/Project01/project01_01.py
+++ Projects/Project01/project01_01.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python
+
+
+
 import requests
 #Assigning a variable to the API Key
 token = "5f86f11d709364014dc941832b05e60a2ce498d3"
@@ -8,4 +12,26 @@ response = requests.get(url, params={"token": token, "keyword": "montreal"})
 #Give back the information as a JSON format
 results = response.json()
 #Prints the result in the terminal
-print(results)
+# print(results)
+
+## Beginning of the experience
+
+# print(results.keys())
+responseData = results["data"]
+# print(type(responseData))
+
+print(responseData)
+
+# for item in responseData:
+#     print(item["station"])
+
+
+
+
+# print("Welcome, please type in your name")
+# username = input()
+# print(f"Hello, {username}, what's the weather like outside?")
+# if input() == "sunny":
+#     print(f"Ilove sunny days do you, {username}")
+# if input()== "cloudy":
+#     print(":(")
