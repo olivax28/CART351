0a72980370a04c4d7556c424b2e24c42b1e0f457
olivax28 <olivax28@outlook.com>
2025-10-20 17:23:35 -0400

    almsot done exercise 02

---
+++ oliva-axiuk_Julia-axiuk_exercise02/exercise-two.py
@@ -0,0 +1,44 @@
+from flask import Flask,render_template,request
+import os
+app = Flask(__name__)
+
+
+# the default route
+@app.route("/")
+def index():
+      return render_template("index.html")
+
+#*************************************************
+
+#Task: Variables and JinJa Templates
+@app.route("/t1")
+def t1():
+      the_topic = "donuts"
+      number_of_donuts = 28
+      donut_data= {
+      "flavours":["Regular", "Chocolate", "Blueberry", "Devil's Food"],
+      "toppings": ["None","Glazed","Sugar","Powdered Sugar",
+                   "Chocolate with Sprinkles","Chocolate","Maple"]
+                   }
+      
+      icecream_flavors = ["Vanilla","Raspberry","Cherry", "Lemon"]
+      return render_template("t1.html", flavours = donut_data["flavours"],toppings = donut_data["toppings"],
+                             passedImg = "donut_a.png",passedImg02 = "donut_sprinkles.png", passedImgHead = "donut_pink.png", icecream = icecream_flavors)
+
+#*************************************************
+
+#Task: HTML Form get & Data 
+@app.route("/t2")
+def t2():
+    return render_template("t2.html")
+
+@app.route("/thank_you_t2")
+def thank_you_t2():
+    app.logger.info(request.args)
+    return render_template("thank_you_t2.html", OrderSummary = f'{request.args["o_name"]} , {request.args["o_type"]} , {request.args["address"]} , {request.args["o_details"]}'
+    )
+
+#*************************************************
+
+#run
+app.run(debug=True)
