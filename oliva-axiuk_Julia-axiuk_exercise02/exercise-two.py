c5d9cd03391cdd2dc5ba339967aa07da2fd15add
olivax28 <olivax28@outlook.com>
2025-10-22 20:44:20 -0400

    fixed images

--- oliva-axiuk_Julia-axiuk_exercise02/exercise-two.py
+++ oliva-axiuk_Julia-axiuk_exercise02/exercise-two.py
@@ -23,7 +23,7 @@ def t1():
     #   3,4
       icecream_flavors = ["Vanilla","Raspberry","Cherry", "Lemon"]
       return render_template("t1.html", flavours = donut_data["flavours"],toppings = donut_data["toppings"],
-                             passedImg = "donut_a.png",passedImg02 = "donut_sprinkles.png", passedImgHead = "donut_pink.png", icecream = icecream_flavors, topic = the_topic, max =number_of_donuts)
+                             passedImg = "donut_a.png",passedImg02 = "donut_sprinkles.png", passedImgHead = "donut_pink.png",passedImgHead01 = "donut_f.png",passedImgHead_c = "donut_c.png" ,icecream = icecream_flavors, topic = the_topic, max =number_of_donuts)
 
 #*************************************************
 # TASK 03
@@ -39,7 +39,7 @@ def thank_you_t2():
     string = f'{request.args["o_name"]}{request.args["o_type"]}{request.args["address"]}{request.args["o_details"]}'
     # replacement of vowels
     replacement = string.replace("a", "*").replace("e", "*").replace("i", "*").replace("o", "*").replace("u", "*")
-    return render_template("thank_you_t2.html", OrderSummary = replacement
+    return render_template("thank_you_t2.html", OrderSummary = replacement, star_img = "star.png",donut_f_img = "donut_f.png"
     )
 
 #*************************************************
