fa8bb6bfc0b123baa9ba93a2c6457d11e2d4c5e6
olivax28 <olivax28@outlook.com>
2025-11-08 21:29:00 -0500

    Got text to show up and be clicked through in the javascript

--- Projects/project02_javascript/js/script.js
+++ Projects/project02_javascript/js/script.js
@@ -8,16 +8,26 @@
 "use strict";
 
 
-
-
-
-
 const fortuneText = [
     "Fortune 01 has a random fill",
+    "Fortune 01 has two lines",
+    "This is the start of Fortune 02",
     "Fortune 02 has a random fill",
     "Fortune 03 has a random fill",
-    "Fortune 04 has a random fill"
+    "Fortune 03 has three lines",
+    "End pf Fortune 03"
 ]
+
+let speechBox = {
+    x: 50,
+    y: 150,
+    width: 300,
+    height: 80,
+    padding: 20,
+    fontSize: 18
+};
+
+let dialogueIndex = 0;
 
 
 // function preload() {
@@ -49,17 +59,42 @@ function draw() {
 
     if (state === "fortune") {
         fortune();
+        showFortune(fortuneText);
+
     }
 }
 
 function fortune() {
-    background(0, 250, 0);
+    background(59, 13, 79);
+    //select random fortune on a click
+
+}
+
+function mouseClicked() {
+    if (state === "fortune") {
+        dialogueIndex++;
+    }
 }
 
 
 
+function showFortune(dialogue) {
 
+    // The background box
+    push();
+    fill(0);
+    stroke(255);
+    strokeWeight(3);
+    rect(speechBox.x, speechBox.y, speechBox.width, speechBox.height);
+    pop();
 
+    //the dialogue itself
+    push();
+    fill(255);
+    textSize(18);
+    text(dialogue[dialogueIndex], speechBox.x + speechBox.padding, speechBox.y + speechBox.padding, speechBox.width - 2 * speechBox.padding, speechBox.height - 2 * speechBox.padding);
+    pop();
+}
 
 
 
